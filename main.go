d0bc45c1d8cec930221cb0c789f0fe948e2074a0
Maciej Dyminkiewicz <maciej.dyminkiewicz@gmail.com>
2022-02-19 13:17:00 +0100

    [heroku deploy] dynamically  determine port

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
-    "github.com/gofiber/fiber/v2"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-    // Start a new fiber app
-    app := fiber.New()
+	// Start a new fiber app
+	app := fiber.New()
+	port := os.Getenv("PORT")
 
-    // Send a string back for GET calls to the endpoint "/"
-    app.Get("/", func(c *fiber.Ctx) error {
-        err := c.SendString("And the API is UP!")
-        return err
-    })
+	// Send a string back for GET calls to the endpoint "/"
+	app.Get("/", func(c *fiber.Ctx) error {
+		err := c.SendString("And the API is UP!")
+		return err
+	})
 
-    // Listen on PORT 3000
-    app.Listen(":3000")
+	// Listen on PORT 3000
+	app.Listen(port)
 }
